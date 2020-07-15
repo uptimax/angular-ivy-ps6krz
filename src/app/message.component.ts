fd8c1308a81b4031b5f153097916236971e11a04
uptimax <festusmaxwell7@gmail.com>
2020-07-15 16:17:15 +0100

    2 files modified

--- src/app/message.component.ts
+++ src/app/message.component.ts
@@ -3,8 +3,12 @@ import {Component,Input} from '@angular/core'
 @Component({
   selector:'msg',
   templateUrl:'message.component.html',
-  styles:['h1{font-family:robot,san-serif']
+  styles:['h1{font-family:robot,san-serif','h2{color:teal']
 })
 export class MessageComponent{
   @Input() emoji: string;
+  awesome = ' awesome🔥🔥🔥🔥';
+  inc(){
+    this.awesome +=  ' 💕👄💝💋'
+  }
 }
