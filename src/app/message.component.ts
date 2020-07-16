5ae60421e6d383b5b86aa7c67256ff2cb6bc648d
uptimax <festusmaxwell7@gmail.com>
2020-07-16 12:36:13 +0100

    1 file modified

--- src/app/message.component.ts
+++ src/app/message.component.ts
@@ -1,25 +1,24 @@
-import {Component,Input} from '@angular/core';
-import { NgForm } from '@angular/forms';
+import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
-  selector:'msg',
-  templateUrl:'message.component.html',
-  styles:['h1{font-family:robot,san-serif','h2{color:teal']
+  selector: 'msg',
+  template: `
+    <form #f="ngForm" (ngSubmit)="onSubmit(f)" novalidate>
+      <input name="first" ngModel required #first="ngModel">
+      <input name="last" ngModel>
+      <button>Submit</button>
+    </form>
+
+    <p>First name value: {{ first.value }}</p>
+    <p>First name valid: {{ first.valid }}</p>
+    <p>Form value: {{ f.value | json }}</p>
+    <p>Form valid: {{ f.valid }}</p>
+  `,
 })
-export class MessageComponent{
-  @Input() emoji: string;
-  awesome = ' awesome🔥🔥🔥🔥';
-  inc(){
-    this.awesome +=  ' 💕👄💝💋'
-  }
-  name;
-  prod_name;
-  quantity = 1;
-  products = ["bread"];
-  addprod(f: NgForm){
-    this.products.push(f);
-  }
-  add(){
-    ++this.quantity
+export class MessageComponent {
+  onSubmit(f: NgForm) {
+    console.log(f.value);  // { first: '', last: '' }
+    console.log(f.valid);  // false
   }
 }
