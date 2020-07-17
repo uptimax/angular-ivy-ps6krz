66b6ac749750061e2efcbcca41cb72eddca7ee5b
uptimax <festusmaxwell7@gmail.com>
2020-07-17 13:16:12 +0100

    2 files modified

--- src/app/message.component.ts
+++ src/app/message.component.ts
@@ -1,24 +1,13 @@
-import {Component} from '@angular/core';
-import {NgForm} from '@angular/forms';
+import Component from '@angular/core'
+import NgForm from '@angular/forms'
 
-@Component({
-  selector: 'msg',
-  template: `
-    <form #f="ngForm" (ngSubmit)="onSubmit(f)" novalidate>
-      <input name="first" ngModel required #first="ngModel">
-      <input name="last" ngModel>
-      <button>Submit</button>
-    </form>
-
-    <p>First name value: {{ first.value }}</p>
-    <p>First name valid: {{ first.valid }}</p>
-    <p>Form value: {{ f.value | json }}</p>
-    <p>Form valid: {{ f.valid }}</p>
-  `,
+Component({
+  selector:"form",
+  templateUrl:"message.component.html",
+  styles:[]
 })
-export class MessageComponent {
-  onSubmit(f: NgForm) {
-    console.log(f.value);  // { first: '', last: '' }
-    console.log(f.valid);  // false
+export class MessageComponent{
+  onSubmit(f: NgForm){
+    console.log(f);
   }
 }
